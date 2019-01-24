eed4496426fa33484316c6dc1e9d236be96809f4
Pál Monoczki <pal.monoczki@codecool.com>
2019-01-24 16:27:23 +0100

    init

---
+++ CustomStyleCopRuleDemo/InstanceVariablesVarPrefix.cs
@@ -0,0 +1,34 @@
+﻿
+namespace DotNetExtensions.StyleCop.Rules
+{
+    using Microsoft.StyleCop;
+    using Microsoft.StyleCop.CSharp;
+
+    /// <summary>
+    /// This StyleCop Rule makes sure that instance variables are prefixed with an underscore.
+    /// </summary>
+    [SourceAnalyzer(typeof(CsParser))]
+    public class InstanceVariablesVarPrefix : SourceAnalyzer
+    {
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            CsDocument csdocument = (CsDocument)document;
+            if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
+            {
+                csdocument.WalkDocument(new CodeWalkerElementVisitor<object>(this.VisitElement), null, null);
+            }
+
+        }
+
+        private bool VisitElement(CsElement element, CsElement parentElement, object context)
+        {
+            if (!element.Generated && element.ElementType == ElementType.Field && element.Declaration.Name.StartsWith("var"))
+            {
+                AddViolation(element, "InstanceVariablesVarPrefix");
+
+            }
+
+            return true;
+        }
+    }
+}
